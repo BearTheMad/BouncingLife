6219881debc960616b1a850d45877339b2ea4c7f
XieChaohui <36781969+XieChaohui@users.noreply.github.com>
2020-08-17 15:41:13 +0800

    //

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,2 +1,24 @@
+import java.io.File;
+import java.nio.charset.Charset;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Test {
+    public static void main(String[] args) {
+        File file=new File("C:\\test\\test1.csv\\20888112769372170156_20200722_业务明细.csv");
+        System.out.println(file.getName());
+System.out.println(Charset.defaultCharset());
+    }
+
 }
+
+class Father{
+    public Father(String str){
+
+    }
+}
+ class Son extends  Father{
+    public Son(){
+        super("");
+    }
+ }
